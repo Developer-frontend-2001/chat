cb0a2efb40da0b0da990695c1c92a32b8c04c30f
Developer-frontend-2001 <emailkerak@gmail.com>
2021-08-12 22:54:54 +0500

    commit

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "root");
-define("DB_DATABASE", "coronatest");
+define("DB_DATABASE", "");
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
