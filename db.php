8bcd535a8165a415a1b06dfd5763da982950161e
Developer-frontend-2001 <emailkerak@gmail.com>
2021-08-12 22:43:27 +0500

    first-commit

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
+define("DB_DATABASE", "coronatest");
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+
+// Bazaga ulanganligini tekshirish
+if ($conn->connect_error) {
+    die("Bazaga ulana olmadi: " . $conn->connect_error);
+}
+?>
